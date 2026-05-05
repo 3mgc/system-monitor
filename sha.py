82d6d089e6a58b36d6d34126cfcdee99b88c1cdb
3mgc <emilianojgarcia.23@gmail.com>
2026-05-04 21:56:49 -0600

    idk :<

--- sha.py
+++ sha.py
@@ -1,6 +1,19 @@
 from flask import Flask, jsonify
 import json
+import socket
 app = Flask(__name__)
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('8.8.8.8', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
 
 @app.route("/")
 def stats():
@@ -8,7 +21,11 @@ def stats():
         data = json.load(f)
     return jsonify(data)
 
+print(f"Connect to: http://{get_ip()}:8000")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    
+    app.run(host='0.0.0.0', port=8000, debug=False, use_reloader=False )
+    
 
 
